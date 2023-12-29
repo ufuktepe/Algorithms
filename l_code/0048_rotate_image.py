edbe918494eacb854a8f937aa227ef821b916034
Burak Ufuktepe <buu837@g.harvard.edu>
2023-12-29 15:40:45 -0800

    added problems.

--- l_code/0048_rotate_image.py
+++ l_code/0048_rotate_image.py
@@ -1,19 +1,31 @@
 def rotate_image(matrix):
     n = len(matrix)
+    q = n - 1
+    index = 0
 
-    layers = n // 2
+    while q > 1:
+        for r in range(q):
+            i = index
+            j = index + r
+            prev = matrix[index][index + r]
+            for k in range(4):
+                temp = matrix[j][n - 1 - i]
+                matrix[j][n - 1 - i] = prev
+                prev = temp
 
-    start = 0
-    end = n - 1
+                i, j = j, n - 1 - i
 
-    for layer in range(layers):
-        j = layer
-        for i in range(start, end + 1):
-            matrix[j][n - i - 1] = matrix[i][j]
+        index += 1
+        q -= 2
 
 
-        start += 1
-        end -= 1
-
+def test():
+    matrix = [[1, 2, 3],
+              [4, 5, 6],
+              [7, 8, 9]]
+    rotate_image(matrix)
+    assert matrix == [[7, 4, 1],
+                      [8, 5, 2],
+                      [9, 6, 3]]
 
 
